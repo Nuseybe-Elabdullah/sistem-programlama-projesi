c70c9d47b00345d5e416d007d5eef9a82e320ab5
Nuseybe Elabdullah <nuseybe.elabdullah@ogr.sakarya.edu.tr>
2026-05-24 15:33:10 +0300

    Add files via upload

---
+++ file_utils.h
@@ -0,0 +1,8 @@
+#ifndef FILE_UTILS_H
+#define FILE_UTILS_H
+
+#include <sys/types.h>
+
+int create_target_dir(const char *dir_path);
+
+#endif
---
+++ validation.h
@@ -0,0 +1,13 @@
+#ifndef VALIDATION_H
+#define VALIDATION_H
+
+#include <stddef.h>
+
+#define MAX_FILES 32
+#define MAX_TOTAL_SIZE (200 * 1024 * 1024)
+#define METADATA_SIZE_LEN 10
+
+int is_ascii_text(const char *filepath);
+int is_safe_filename(const char *filename);
+
+#endif
